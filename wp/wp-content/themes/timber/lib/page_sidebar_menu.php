aa8d4cb72a21fa3b8ebbd025d4bdd192249ea299
Ben Coleman <ben@fffunction.co>
2019-05-01 21:06:28 +0100

    About and What we do templates

--- wp/wp-content/themes/timber/lib/page_sidebar_menu.php
+++ wp/wp-content/themes/timber/lib/page_sidebar_menu.php
@@ -12,39 +12,22 @@ function get_page_sidebar_menu($post, $include_top = true, $postType = null, $de
 
     $args = array(
         'child_of'      => $top_id,
-        'depth'         => $depth, // Only works with wp_list_pages()
+        'depth'         => $depth,
         'hierarchical'  => true,
         'post_type'     => 'page',
         'post_status'   => 'publish',
         'sort_column'   => 'menu_order, post_title',
         'echo'          => 0,
         'title_li'      => 0,
-        'link_before'   => '<i role="presentation" aria-hidden="true" class="ico ico--16 ico-arrow-lavender"></i> '
+        'link_before'   => ''
     );
 
     $submenu = wp_list_pages($args);
 
-    $submenu = str_replace('<li class="', '<li class="navigation-tertiary__item ', $submenu);
-    $submenu = str_replace('current_page_item', 'current_page_item navigation-tertiary__item--active', $submenu);
-    $submenu = str_replace('page_item_has_children', 'page_item_has_children navigation-tertiary__item--parent', $submenu);
+    $submenu = str_replace('<li class="', '<li class="navigation-secondary__item ', $submenu);
+    $submenu = str_replace('current_page_item', 'current_page_item navigation-secondary__item--active', $submenu);
+    $submenu = str_replace('page_item_has_children', 'page_item_has_children navigation-secondary__item--parent', $submenu);
     $submenu = str_replace('<ul class=\'children\'', '<ul class="children navigation-tertiary--childlist"', $submenu);
-
-    // if ($include_top) {
-    //     $top = array(get_page($top_id));
-    //     $all = array_merge($top, get_pages($args));
-    // }
-    // else {
-    //     $all = get_pages($args);
-    // }
-
-    // $submenu = array_map(function ($r) {
-    //     return array(
-    //         'ID' => $r->ID,
-    //         'link' => get_permalink($r->ID),
-    //         'title' => $r->post_title,
-    //         'post_parent' => $r->post_parent,
-    //     );
-    // }, $all);
 
     return $submenu;
 }
