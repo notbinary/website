70516fa093c452dc9bfdfbd935b6c35e6ed45a66
Ben Coleman <ben@bteam.co>
2019-07-02 23:18:54 +0100

    Made the images for logos the size you'd expect

--- wp/wp-content/themes/timber/page-templates/home.php
+++ wp/wp-content/themes/timber/page-templates/home.php
@@ -8,6 +8,8 @@ global $params;
 
 $context['page_links'] = get_field('page_links');
 
+$context['logos'] = get_field('logos');
+
 // echo '<pre>'; print_r($context['page_links']); echo '</pre>';
 
 Timber::render('page/home.html', $context);
