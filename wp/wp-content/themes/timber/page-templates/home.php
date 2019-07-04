cd699dad07581954c387ce603608fd8008242f65
Ben Coleman <ben@bteam.co>
2019-07-04 16:16:44 +0100

    completed WP integration

--- wp/wp-content/themes/timber/page-templates/home.php
+++ wp/wp-content/themes/timber/page-templates/home.php
@@ -10,6 +10,4 @@ $context['page_links'] = get_field('page_links');
 
 $context['logos'] = get_field('logos');
 
-// echo '<pre>'; print_r($context['page_links']); echo '</pre>';
-
 Timber::render('page/home.html', $context);
